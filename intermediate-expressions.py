a5aeaba9d3125971c875cfefd8fef3e97a4153b4
Marguerite Blair <marguerite.blair@gmail.com>
2020-09-15 18:31:58 -0400

    created to python programs, one to print your name once you input it, and another which converts between floors

---
+++ intermediate-expressions.py
@@ -0,0 +1,15 @@
+sval = '123'
+type(sval)
+
+ival = int(sval)
+type(ival)
+
+print(ival + 1)
+
+nam = input('Who are you? ')
+print('Welcome', nam)
+
+# elevator convention application
+inp = input('European floor? ')
+usf = int(inp) + 1
+print('US Floor', usf)
