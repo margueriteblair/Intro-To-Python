7a4ae12ec498661e0711cd48ace692b01b73ee53
Marguerite Blair <marguerite.blair@gmail.com>
2020-09-15 18:41:52 -0400

    reviewed python conditionals and whitespacing protocols

---
+++ conditional-execution.py
@@ -0,0 +1,25 @@
+x = 5
+if x < 10:
+    print('Smaller')
+if x > 20:
+    print('Bigger')
+
+print('Done')
+
+x = 5
+print('Before 5')
+if x == 5:
+    print('X is 5')
+    print('Is still 5')
+    print('Third 5')
+print('Afterwards 5')
+print('Before 6')
+if x == 6: 
+    print('Is 6')
+    print('Is still 6')
+    print("Third 6")
+print('Afterwards 6')
+
+if 0 == x:
+    if y == 10:
+        print('Yes')
