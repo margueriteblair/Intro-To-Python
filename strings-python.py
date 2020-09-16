b4bdc6355f7121ff2e8a5674b1c8d0c828afcea9
Marguerite Blair <marguerite.blair@gmail.com>
2020-09-16 11:07:25 -0400

    for loop and while loop to iterate through strings and print the letters

---
+++ strings-python.py
@@ -0,0 +1,18 @@
+fruit = "banana"
+for letter in fruit:
+    print(letter)
+    print(len(fruit))
+
+fruit = "coconut"
+index = 0
+while index < len(fruit):
+    letter = fruit[index]
+    print(index, letter)
+    index = index + 1
+
+
+#python for loop that prints out all the letters in a string
+#coconut and banana are of the same length hehe!
+#the less code you write, the less chance there is that you can make a mistake!
+
+#"in" keyword in python
