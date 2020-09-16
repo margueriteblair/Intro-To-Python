f13d376311a489c30519e98e184cee44b8f94764
Marguerite Blair <marguerite.blair@gmail.com>
2020-09-15 21:06:02 -0400

    find the largest number in python

---
+++ loop-idioms.py
@@ -0,0 +1,11 @@
+#we want to solve a problem with a loop
+#we want the largest or the smallest
+
+#what is the largest number algo
+
+biggest = 0
+for i in [3, 41, 12,9, 74, 15]:
+    if i > biggest:
+        biggest = i
+
+print(biggest)
