af726ebced5c06815e6f5c703e2c436c1e3cc665
Marguerite Blair <marguerite.blair@gmail.com>
2020-09-15 20:37:41 -0400

    greeting function to give different greetings based on the passed in language code

---
+++ build-functions.py
@@ -0,0 +1,17 @@
+def print_lyrics():
+    print("I am a lumberjac, and I'm okay")
+    print("I sleep all night and I work all day")
+
+print_lyrics()
+
+def greet(lang):
+    if lang == 'es':
+        print('Hola')
+    elif lang == 'fr':
+        print('Bounjour')
+    else:
+        print('Hello')
+
+greet('no')
+
+#def defines a function butdoes not call/invoke
