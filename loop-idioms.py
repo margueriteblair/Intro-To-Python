7a8de139a3e3bdca2f1927d11113bcd9d1fcffa4
Marguerite Blair <marguerite.blair@gmail.com>
2020-09-15 21:13:02 -0400

    for loop to find the smallest

--- loop-idioms.py
+++ loop-idioms.py
@@ -9,3 +9,11 @@ for i in [3, 41, 12,9, 74, 15]:
         biggest = i
 
 print(biggest)
+
+smallest = None
+print("Before:", smallest)
+for itervar in [3, 41, 12, 9, 74, 15]:
+    if smallest is None or itervar < smallest:
+        smallest = itervar
+    print("Loop:", itervar, smallest)
+print("Smallest:", smallest)
