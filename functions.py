5559e8a0d2a6a9715103f90f33f5aaf7a6feb41c
Marguerite Blair <marguerite.blair@gmail.com>
2020-09-15 20:32:01 -0400

    used built in python functions

---
+++ functions.py
@@ -0,0 +1,11 @@
+#python has many built in functions like input
+#def keyword to define the function
+#then you call/invoke the function 
+#min and max are built in functions
+
+answer = input("Enter a phrase... ")
+big = max(answer)
+small = min(answer)
+
+print(big, small)
+
