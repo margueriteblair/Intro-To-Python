a5e29a2ba3172ef605e0bfe1346c9ff539d1f12a
Marguerite Blair <marguerite.blair@gmail.com>
2020-09-16 12:19:19 -0400

    built in list methods

---
+++ navigating-lists.py
@@ -0,0 +1,21 @@
+#python has an object oriented approach 
+
+a = [1, 2, 3]
+b = [4, 5, 6]
+c = a + b
+print(c)
+#neither a or b are changed
+
+x = list()
+type(x)
+dir(x)
+
+#list() makes an empty list and assigns it into a var name
+#stuff.append() is like .push()
+
+nums = [3, 31, 23, 75, 22,99]
+
+print(len(nums))
+print(max(nums))
+print(min(nums))
+print(sum(nums))
